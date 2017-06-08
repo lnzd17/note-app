e9f45f37ec5e6ccb717641d54f73f41b2201ae7e
Lindsay Dages <lindsaykayedutton@gmail.com>
2017-06-08 21:35:47 +0000

    added components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav.js';
+import List from './components/List.js';
+import Note from './components/Note.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Nav />
+        <List />
+        <Note />
       </div>
     );
   }
---
+++ src/components/Note.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Note extends React.Component {
+  render() {
+    return(
+      <div className="note-container">Note Component</div>
+
+    );
+  }
+}
+
+export default Note;
